ef093cedf2c637cbec8754d62229f5e2fa0185eb
Đào thế anh <daotheanh58@gmail.com>
2026-04-30 21:01:26 +0700

    UPDATE V1

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 // Lấy URL từ biến môi trường, xóa bỏ mọi dấu ngoặc hoặc khoảng trắng thừa nếu có
-const rawURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
-const BASE_URL = rawURL.replace(/[\[\]]/g, "").trim(); 
+//const rawURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
+//const BASE_URL = rawURL.replace(/[\[\]]/g, "").trim(); 
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
 
-// Kiểm tra URL thực tế trong Console để debug
-console.log("Dự án đang gọi API tại:", BASE_URL);
+// // Kiểm tra URL thực tế trong Console để debug
+// console.log("Dự án đang gọi API tại:", BASE_URL);
 
 api.interceptors.request.use(
   (config) => {
