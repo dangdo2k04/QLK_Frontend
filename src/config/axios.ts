f9aa6c653fa527195fc1f1900075da2e0a579acb
Đào thế anh <daotheanh58@gmail.com>
2026-04-30 17:37:59 +0700

    30/4 UPDATE

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const BASE_URL = (import.meta as any).env.VITE_API_URL || 'http://localhost:5000/api/v1';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
